87b166b47433356786eecf4c90149663a31803ce
calebjo <caleb.ri.jones@gmail.com>
2022-01-17 12:10:31 -0500

    Add component skeletons, config for keys

---
+++ frontend/src/components/question_set/question_set_item.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+class QuestionSetItem extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render(){
+        return(
+            <div className="question-set-item">
+                I AM AN ITEM
+            </div>
+        )
+    }
+}
+
+export default QuestionSetItem;
