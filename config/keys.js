3fe9176f78438ed816e3dd594ed0f3eb46b9e344
Julio Tavarez <90402490+jdtavarez@users.noreply.github.com>
2022-01-17 11:35:58 -0500

    Delete keys.js

--- config/keys.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    mongoURI: "mongodb+srv://admin:hokzuh-sonjib-Gapta8@quizard.hdccc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-    secretOrKey: "$2a$10$8YyMZMKcAi7hPzdWDAo..O79WT9iEsUODLDlc57Kq29fML.7AqdQi"
-}
-
