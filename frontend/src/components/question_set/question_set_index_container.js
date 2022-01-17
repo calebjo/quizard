8c7f96d390c07279d799cd1ed67dcafe250408f8
calebjo <caleb.ri.jones@gmail.com>
2022-01-17 13:31:29 -0500

    Add skeleton for question set index container

---
+++ frontend/src/components/question_set/question_set_index_container.js
@@ -0,0 +1,15 @@
+import { connect } from "react-redux";
+import QuestionSetIndex from './question_set_index'
+
+// redux state
+// more(?)
+const mSTP = (state, ownProps) => ({
+
+});
+
+// fetch all question sets
+const mDTP = dispatch => ({
+
+});
+
+export default connect(mSTP, mDTP)(QuestionSetIndex);
