eab509e4469ab1a38f5dc9f82944c222d62202b0
calebjo <caleb.ri.jones@gmail.com>
2022-01-18 12:28:43 -0500

    Fix redux cycle for getting questions and question sets

--- frontend/src/components/question_set/question_set_item.jsx
+++ frontend/src/components/question_set/question_set_item.jsx
@@ -3,14 +3,32 @@ import React from 'react'
 class QuestionSetItem extends React.Component {
     constructor(props) {
         super(props)
+        this.state = {
+            questions: []
+        }
+        this.startGame = this.startGame.bind(this)
+    }
+
+    componentDidMount() {
+        this.props.fetchSetQuestions(this.props.questionSet._id).then((response) => {
+            this.setState({
+                questions: response.questions.data
+            })
+        })
     }
 
     startGame() {
         // SKELETON -- change to start a game when built
-        console.log(`Starting a game from a ${this.props.category} set!`)
+        console.log(`Starting a game from a ${this.props.questionSet.category} set!`)
     }
 
     render(){
+        // Pluralize question number text
+        const questionNumText = this.state.questions.length > 1 || this.state.questions.length === 0 ? (
+            this.state.questions.length.toString() + " questions"
+        ) : (
+            this.state.questions.length.toString() + " question"
+        )
         return(
             <div className="question-set-item">
                 <div className="question-set-item__container">
@@ -19,7 +37,7 @@ class QuestionSetItem extends React.Component {
                     </div>
                     <div className="question-set-item__details-container">
                         <div className="question-set-item__question-number">
-                            {this.props.questions.length} questions
+                            { questionNumText }
                         </div>
                         <div className="question-set-item__category">
                             {this.props.questionSet.category}
