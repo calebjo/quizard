a8342e99dbc9e47bef148efee9af58c82cec1e55
calebjo <caleb.ri.jones@gmail.com>
2022-01-18 10:34:45 -0500

    Reset app.js routes (oopsies merge conflicty)

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -19,12 +19,7 @@ const App = () => (
         <Route path="/edit-profile" component={SideNavContainer}/>
 
         <Switch>
-<<<<<<< HEAD
             <Route exact path="/edit-profile" component={EditUserFormContainer} />
-=======
-            <Route exact path="/quiz-sets" component={QuestionSetIndexContainer} />
-            <Route exact path="/users/:id/edit" component={EditUserFormContainer} />
->>>>>>> fa7c3bd (Adjust styling, components)
             <Route exact path="/users/:id" component={UserShowContainer} />
             <Route exact path="/login" component={LoginFormContainer} />
             <Route exact path="/signup" component={SignupFormContainer} />
