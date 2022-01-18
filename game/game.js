01469d87937ae12080f2cfd7ad5f1126e0e8dece
jdtavarez <juliodtavarez@gmail.com>
2022-01-18 16:33:24 -0500

    game logic complete maybe

--- game/game.js
+++ game/game.js
@@ -1,20 +1,24 @@
+const { response, response } = require("express");
+
 class Game {
     constructor(questions, players) {
-        this.rounds = questions.length;
-        this.questions = questions;
-        this.players = players;
+        // questions should be an array of question objects
+        // players should ONE OBJECT { _id: username, _id: username, _id: username }
+        this.totalRounds = questions.length;
+        this.round = 0;
+        this.questions = Game.normalizeQuestions(questions);
         this.activePlayers = players;
-        this.inactivePlayers = [];
-
+        this.inactivePlayers = {};
         this.incorrectAnswersHelper = this.incorrectAnswersHelper.bind(this);
+        this.playRound = this.playRound.bind(this);
+        this.gameOver = this.gameOver.bind(this);
     }
 
     static normalizeQuestions(questions) {
-        // questions should be an array of question objects
-        // Each question object should contain { question, incorrectAnswers, correctAnswer, category }
+        // Each question object should contain { question, incorrectAnswers, correctAnswer, category, type }
         let normalizedQuestions = [];
         questions.forEach(question => {
-            const _normalizedQuestion = {};
+            let _normalizedQuestion = {};
             _normalizedQuestion['correctAnswer'] = question.correctAnswer;
             if (question.incorrectAnswers.length < 4) {
                 _normalizedQuestion['incorrectAnswers'] = question.incorrectAnswers;
@@ -23,9 +27,11 @@ class Game {
             }
             normalizedQuestions.push(_normalizedQuestion);
         })
+        return normalizedQuestions
     }
 
     incorrectAnswersHelper(answersArray) {
+        // In the event a question has more than 3 incorrect answers this function randomly selects 3
         let newAnswersArray = [];
         let indexStore = [];
         const totalOptions = answersArray.length;
@@ -41,4 +47,38 @@ class Game {
 
         return newAnswersArray;
     }
+
+    playRound(question, playerResponses) {
+    // question should be a singular question object
+    // responses should be an array of 'response objects'
+    // e.g. [{ playerId: response }, { playerId: response }, { playerId: response }]
+        const correctAnswer = question.correctAnswer;
+        let removals = [];
+
+        playerResponses.forEach(responseObj => {
+            const player = Object.keys(responseObj)[0];
+            const response = Object.values(responseObj)[0];
+            if (response !== correctAnswer) {
+                removals.push(player);
+            }
+        })
+
+        if (removals.length > 0) {
+            removals.forEach(player => {
+                this.inactivePlayers[player] = this.activePlayers[player];
+                delete this.activePlayers[player];
+            })
+        }
+
+        round++;
+        return true;
+    }
+
+    gameOver() {
+        let result; 
+        this.round === this.totalRounds ? result = true : result = false;
+        return result;
+    }
 }
+
+
