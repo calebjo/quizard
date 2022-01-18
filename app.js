d59f5c304e90b50c2cd5a0a0d48e56fbf38debd0
jdtavarez <juliodtavarez@gmail.com>
2022-01-18 16:38:16 -0500

    remove extraneous files from app.js

--- app.js
+++ app.js
@@ -29,29 +29,3 @@ app.use("/api/game_records/", gameRecords);
 
 const port = process.env.PORT || 4000;
 app.listen(port, () => console.log(`Server is running on port ${port}`))
-
-
-// const art = require('./trivia_seeds/art');
-// const film = require('./trivia_seeds/film');
-// const food = require('./trivia_seeds/food');
-// const geography = require('./trivia_seeds/geography');
-// const gnk = require('./trivia_seeds/gnk');
-// const history = require('./trivia_seeds/history');
-// const music = require('./trivia_seeds/music');
-// const science = require('./trivia_seeds/science');
-// const society = require('./trivia_seeds/society');
-// const sport = require('./trivia_seeds/sport');
-
-// const Question = require('./models/Question');
-
-// Question.insertMany(art);
-// Question.insertMany(film);
-// Question.insertMany(food);
-// Question.insertMany(art);
-// Question.insertMany(geography);
-// Question.insertMany(gnk);
-// Question.insertMany(history);
-// Question.insertMany(music);
-// Question.insertMany(science);
-// Question.insertMany(society);
-// Question.insertMany(sport);
