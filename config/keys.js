3edb9d352e125632f086ad10ec5113c9978c2c97
calebjo <caleb.ri.jones@gmail.com>
2022-01-18 14:44:12 -0500

    Add back-end for heroku push

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
-module.exports = {
-    mongoURI: "mongodb+srv://admin:hokzuh-sonjib-Gapta8@quizard.hdccc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-    secretOrKey: "$2a$10$8YyMZMKcAi7hPzdWDAo..O79WT9iEsUODLDlc57Kq29fML.7AqdQi"
+if (process.env.NODE_ENV === 'production') {
+    module.exports = require('./keys_prod');
+} else {
+    module.exports = require('./keys_dev');
 }
