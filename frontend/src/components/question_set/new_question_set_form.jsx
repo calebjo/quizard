e5bec6d59ec664f46607f52409bcf129de3b3c57
virgknight <virginiaknight888@gmail.com>
2022-01-18 20:09:39 -0700

    A bit of preliminary styling for new q set form, no functionality yet tho

--- frontend/src/components/question_set/new_question_set_form.jsx
+++ frontend/src/components/question_set/new_question_set_form.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "./new_question_set_form.scss";
 
 class NewQuestionSetForm extends React.Component {
 
@@ -7,7 +8,13 @@ class NewQuestionSetForm extends React.Component {
     }
 
     render () {
-        return (<p>New QSet form yay!!!</p>);
+        return (
+            <div className="with-nav new-qset-form-bg">
+                <div className="new-qset-form">
+                    <p>hi!!!</p>
+                </div>
+            </div>
+        );
     }
 }
 
